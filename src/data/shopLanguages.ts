0cac0255f916676372f11b3d5edbf16294c8b02a
Eddev92 <edwarllanca@hotmail.com>
2020-12-09 03:09:00 -0500

    initial commit with template

---
+++ src/data/shopLanguages.ts
@@ -0,0 +1,30 @@
+// application
+import { ILanguage } from '~/interfaces/language';
+
+const dataShopLanguages: ILanguage[] = [
+    {
+        locale: 'en',
+        code: 'en',
+        name: 'English',
+        icon: '/images/languages/language-1.png',
+        direction: 'ltr',
+    },
+    {
+        locale: 'es',
+        code: 'ru',
+        name: 'Español',
+        icon: '/images/languages/language-2.png',
+        direction: 'ltr',
+    },
+    // {
+    //     locale: 'ar',
+    //     code: 'ar',
+    //     name: 'RTL',
+    //     icon: '/images/languages/language-3.png',
+    //     direction: 'rtl',
+    // },
+];
+
+export const dataShopDefaultLocale = 'en';
+
+export default dataShopLanguages;
